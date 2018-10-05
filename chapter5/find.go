2d0f4ba9f3edcd439bd76d673a2b39a5f2a4f542
haridutt <ashariduttparashar@gmail.com>
2018-10-05 11:49:28 +0530

    alternative of find utility in unix

---
+++ chapter5/find.go
@@ -0,0 +1,40 @@
+package main 
+
+import (
+
+	"fmt"
+	"path/filepath"
+	"os"
+)
+
+func WalkFunction(path string, info os.FileInfo, err error) error {
+
+	_, err = os.Stat(path)
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(path)
+	return nil
+}
+
+func main() {
+
+	args := os.Args
+
+	if len(args) != 2 {
+		fmt.Println("enter valid no of arguments")
+		os.Exit(1)
+	}
+
+	Path := args[1]
+
+	err := filepath.Walk(Path, WalkFunction)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+}
