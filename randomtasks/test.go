e2275ef1b10f9bd8b950b6677911f860456ad581
haridutt <ashariduttparashar@gmail.com>
2019-06-03 22:53:55 +0530

    initial commit

---
+++ randomtasks/test.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+func read(s string) int {
+
+	var r io.Reader
+	r, err := os.Open(s)
+	check(err)
+	p := make([]byte, 8)
+	n, err := r.Read(p)
+	check(err)
+	return n
+}
+
+// func write(s string, i []byte) {
+
+// 	file, err := os.OpenFile(s, os.O_RDWR, 0644)
+
+// 	check(err)
+// 	defer file.Close()
+
+// 	file.WriteAt(i, 0) // Write at 0 beginning
+
+// }
+
+func main() {
+
+	args := os.Args
+	cloud, err := strconv.Atoi(args[1])
+	check(err)
+	switch cloud {
+	case 1:
+		key := read("aws.txt")
+		// data := binary.BigEndian.Uint64(key)
+		// fmt.Println("key for aws :", data+1)
+		fmt.Println("key for aws :", key)
+		// write("aws.txt", key)
+
+	case 2:
+
+		// key := read("gcp.txt")
+		// data := binary.BigEndian.Uint16(key)
+		// fmt.Println("key for gcp :", data+1)
+		// write("gcp.txt", key)
+		// write("aws.txt", key)
+		// io.write
+
+	case 3:
+
+		// key := read("alicloud.txt")
+		// data := binary.BigEndian.Uint16(key)
+		// fmt.Println("key for alicloud :", data+1)
+		// write("alicloud.txt", key)
+
+	}
+
+}
